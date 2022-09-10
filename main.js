c65ff7aeb5c21e6d1f68470d374ecd4371634eab
Bruno Cocco <brunoe.cocco@gmail.com>
2022-09-10 03:32:52 -0300

    iniciando proyecto estacionamiento

---
+++ main.js
@@ -0,0 +1,15 @@
+//TODO:
+// vamos a hacer un sistema para un estacionamiento de autos.
+// definir posiciones.
+// seleccionar una posicion y crear un objeto para entregar el ticket
+//crear la card de pa persona y que esta empiece a correr el tiempo corresponediente para poder computar el cobro.
+//opcion cobrar.
+//cerrar posicion y que vuelva a estar disponible.
+
+class Usuario{
+    constructor(nombre,patente,horaInicio){
+        this.name = nombre,
+        this.patenta = patente,
+        this.time = horaInicio
+    }
+}
