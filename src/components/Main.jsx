58e69d2b22b7773da868fee08f92fba7e7dbe258
Bruno Cocco <brunoe.cocco@gmail.com>
2025-12-01 15:11:27 +0100

    modificando arbol de trabajo

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,26 +12,22 @@ function Main({ autos }) {
     <>
       <h1>Lista de Autos</h1>
 
-      {Array.isArray(autos) && autos.length > 0 ? (
-        autos.map((auto, i) => {
-          console.log("AUTOS:", autos);
-          const minutos = Math.floor((Date.now() - auto.inicio) / 60000);
-          const segundos = Math.floor(((Date.now() - auto.inicio) / 1000) % 60);
+      {autos.map((auto, i) => {
+        console.log("AUTOS:", autos);
+        const minutos = Math.floor((Date.now() - auto.inicio) / 60000);
+        const segundos = Math.floor(((Date.now() - auto.inicio) / 1000) % 60);
 
-          return (
-            <div key={i} className="auto-card">
-              <h2>Patente: {auto.patente}</h2>
-              <h3>Tipo: {auto.tipo}</h3>
-              <p>
-                Tiempo transcurrido: {minutos} min {segundos} s
-              </p>
-              <hr />
-            </div>
-          );
-        })
-      ) : (
-        <p>No hay autos registrados todavía.</p>
-      )}
+        return (
+          <div key={i} className="auto-card">
+            <h2>Patente: {auto.patente}</h2>
+            <h3>Tipo: {auto.tipo}</h3>
+            <p>
+              Tiempo transcurrido: {minutos} min {segundos} s
+            </p>
+            <hr />
+          </div>
+        );
+      })}
     </>
   );
 }
--- src/utils/impresora.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs'
-
-function impresora(data){
-    const contenido = data;
-    fs.writeFileSync('./src/hooks/imp1.txt', contenido, 'utf-8')
-}
-
-export default impresora;
